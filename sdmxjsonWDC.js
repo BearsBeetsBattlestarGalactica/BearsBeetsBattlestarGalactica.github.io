1a2c049db230a00ac27fac58e9f27ba510df8024
JoshYEn <JoshYEn@users.noreply.github.com>
2019-11-12 21:42:48 +0100

    Update sdmxjsonWDC.js

--- sdmxjsonWDC.js
+++ sdmxjsonWDC.js
@@ -1,78 +1,127 @@
 (function () {
     var myConnector = tableau.makeConnector();
- 
-   myConnector.getSchema = function (schemaCallback) {
-    
-	var cols = [
-        { id : "cou", alias : "country", dataType : tableau.dataTypeEnum.string },
-        { id : "pol", alias : "pollutant", dataType : tableau.dataTypeEnum.string },
-        { id : "vari", alias : "variable", dataType : tableau.dataTypeEnum.string },
-        { id : "time", alias : "year", dataType : tableau.dataTypeEnum.string },
-        { id : "obs", alias : "observation", dataType : tableau.dataTypeEnum.float }
-    ];
 
-    var tableInfo = {
-        id : Air_Emissions,
-        alias : "Emissions of air pollutants",
-        columns : cols
+    myConnector.getSchema = function (schemaCallback) {
+        var cols = [{
+            id: "cou",
+            alias: "country",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "pol",
+            alias: "pollutant",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "vari",
+            alias: "variable",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "time",
+            alias: "year",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "obs",
+            alias: "observation",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "test",
+            alias: "test",
+            dataType: tableau.dataTypeEnum.string
+        }];
+
+        var tableSchema = {
+            id: "test_DB_schema",
+            alias: "Laaaaaa",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
     };
 
-
-schemaCallback([tableInfo]);
-
-	
-	
-	
-    
-};
- 
     myConnector.getData = function (table, doneCallback) {
-    var tableData = [],
-        cou = "",
-        pol = "",
-        vari = "",
-        time = "",
-        obs = 0;
+        // var dateObj = JSON.parse(tableau.connectionData),
+        //     dateString = "startTime=" + dateObj.startDate + "&endTime=" + dateObj.endDate,
+        //     apiCall = "https://stats.oecd.org/SDMX-JSON/data/AIR_GHG/all?" + dateString + "&dimensionAtObservation=allDimensions&detail=dataonly";
 
-    	$.getJSON("https://stats.oecd.org/SDMX-JSON/data/AIR_EMISSIONS/GBR../all?dimensionAtObservation=AllDimensions&detail=dataonly", function (resp) {
-        var obsvs = resp.dataSets[0].observations; 
+        var tableData = [],
+            cou = "",
+            pol = "",
+            vari = "",
+            time = "",
+            obs = 0,
+            test = "";
 
-		
-        for (var i = 0, len = Object.keys(obsvs).length; i < len; i++) {
-			
-			
-			var arrKey = Object.keys(obsvs)[i].split(':')
-			
-            cou = resp.structure.dimensions.observation[0].values[arrKey[0]].name;
-            pol = resp.structure.dimensions.observation[1].values[arrKey[1]].name;
-            vari = resp.structure.dimensions.observation[2].values[arrKey[2]].name;
-            time = resp.structure.dimensions.observation[3].values[arrKey[3]].name;
-            obs = obsvs[Object.keys(obsvs)[i]][0]; 
+        $.getJSON("https://stats.oecd.org/SDMX-JSON/data/AIR_GHG/all?startTime=1990&endTime=2017&dimensionAtObservation=allDimensions&detail=dataonly", function (resp) {
+        // $.getJSON("./test.json", function (resp) {
+            console.log("Hello world!!!!!!");
+            var obsvs = resp.dataSets[0].observations;
 
-            tableData.push({
-                "cou" : cou,
-                "pol" : pol,
-                "vari" : vari,
-                "time" : time,
-                "obs" : obs
-            });
-			
-			
-			
-        }
+            for (var i = 0, len = Object.keys(obsvs).length; i < len; i++) {
+                var arrKey = Object.keys(obsvs)[i].split(':')
 
-        table.appendRows(tableData);
+                console.log("Hello world!");
+                cou = resp.structure.dimensions.observation[0].values[arrKey[0]].name;
+                console.log("cou is:" + cou);
+                pol = resp.structure.dimensions.observation[1].values[arrKey[1]].name;
+                console.log("pol is:" + pol);
+                vari = resp.structure.dimensions.observation[2].values[arrKey[2]].name;
+                console.log("vari is:" + vari);
+                time = resp.structure.dimensions.observation[3].values[arrKey[3]].name;
+                console.log("time is:" + time);
+                obs = obsvs[Object.keys(obsvs)[i]][0];
+                console.log(obs);
+                test = obsvs[Object.keys(obsvs)[i]][1];
 
-        doneCallback();
-    });
-};
+                tableData.push({
+                    "cou": cou,
+                    "pol": pol,
+                    "vari": vari,
+                    "time": time,
+                    "obs": obs,
+                    "test": test,
+
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+
+    };
+
     tableau.registerConnector(myConnector);
-	
-	$(document).ready(function () {
-    $("#submitButton").click(function () {
-        tableau.connectionName = "OECD Emissions of air pollutants";
-        tableau.submit();
+
+
+    // Create event listeners for when the user submits the form
+    // $(document).ready(function() {
+    //     $("#submitButton").click(function() {
+    //         var dateObj = {
+    //             startDate: $('#start-year').val().trim(),
+    //             endDate: $('#end-year').val().trim(),
+    //         };
+
+    //         // Simple date validation: Call the getDate function on the date object created
+    //         function isValidDate(dateStr) {
+    //             var d = new Date(dateStr);
+    //             return !isNaN(d.getDate());
+    //         }
+
+    //         console.log("submit success");
+
+    //         if (isValidDate(dateObj.startDate) && isValidDate(dateObj.endDate)) {
+    //             tableau.connectionData = JSON.stringify(dateObj); // Use this variable to pass data to your getSchema and getData functions
+    //             tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+    //             tableau.submit(); // This sends the connector object to Tableau
+    //         } else {
+    //             $('#errorMsg').html("Enter valid dates. For example, 2016-05-08.");
+    //         }
+    //     });
+    // });
+
+    $(document).ready(function () {
+        $("#submitButton").click(function () {
+            tableau.connectionName = "OECD Emissions of air pollutants";
+            tableau.submit();
+        });
     });
-});
-	
+
 })();
